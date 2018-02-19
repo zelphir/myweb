2bb1e6d7b476d9a8ca50d6ec6caada781d48da73
Roberto Manzella <zelphir@gmail.com>
2018-02-19 16:29:11 +0000

    Wip functions

--- functions/src/instagram/index.js
+++ functions/src/instagram/index.js
@@ -1,6 +1,12 @@
+import * as functions from 'firebase-functions'
+
 const instagram = (req, res) => {
-  res.set({ 'Content-Type': 'application/json' })
-  res.status(200).send(JSON.stringify({ hello: 'world' }))
+  if (req.get('X-Client-ID') !== functions.config().instagram.xclientid) {
+    return res.sendStatus(403)
+  }
+
+  console.log(req.body)
+  res.status(200).end()
 }
 
 export default instagram
