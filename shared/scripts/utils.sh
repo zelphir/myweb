05b7c3b5ca4aa731b437008f8564ce2389d442fc
Roberto Manzella <zelphir@gmail.com>
2018-02-23 00:56:16 +0000

    Add better .env handling

--- shared/scripts/utils.sh
+++ shared/scripts/utils.sh
@@ -1,3 +1,42 @@
 #!/usr/bin/env bash
 
-export $(cat ../.env.production | grep -v ^# | xargs)
+set -e
+
+RED='\033[0;31m'
+NC='\033[0m' # no color
+
+trap 'echo -e "
+${RED}
+================== ERROR ====================
+|                                           |
+|     Please create the .env file           |
+|                                           |
+=============================================
+${NC}
+"' ERR
+[[ -f "../.env" ]]
+trap - ERR
+
+ENV=${1:-production}
+DOTENV="../.env.${ENV}"
+
+if [[ ! -f "$DOTENV" ]]; then
+  DOTENV="../.env"
+fi
+
+export $(cat $DOTENV | grep -v ^# | xargs)
+
+VARIABLES=($(sed -r '/^(\s*#|$)/d;' ../.env.sample))
+
+for v in "${VARIABLES[@]}" ; do
+  var=${v%=}
+  trap 'echo -e "${RED}
+================== ERROR ====================
+
+        ${var} is not set
+
+=============================================
+${NC}"' ERR
+  [[ ! -z ${!var} ]]
+  trap - ERR
+done
