8f79cd3f54a0b913eefea84206fbc076c0951d8e
Roberto Manzella <zelphir@gmail.com>
2018-02-23 16:17:19 +0000

    Simplify docker-build, Update gatsbyjs

--- shared/scripts/docker_build.sh
+++ shared/scripts/docker_build.sh
@@ -5,10 +5,9 @@ source "${BASH_SOURCE%/*}/utils.sh"
 IMAGE="gcr.io/${GOOGLE_PROJECT_ID}/cron"
 
 NODE_ENV=production yarn precron
+
 cd gce-cron
-cp ../package.json .
-mv ../.build .
+cp ../package.json .build
 docker build -t gce-cron .
 docker tag gce-cron $IMAGE
 gcloud docker -- push $IMAGE
-rm -rf .build package.json
