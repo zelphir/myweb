1484aaff0743c8d5ad37e0bb32c4709151a7d496
Roberto Manzella <zelphir@gmail.com>
2018-03-15 00:05:31 +0000

    wip, form fields

--- web/src/components/Contact.js
+++ web/src/components/Contact.js
@@ -1,5 +1,62 @@
 import React from 'react'
+import { Form } from 'react-form'
 
-const Contact = () => <div id="contact">Contact</div>
+import FormField from './FormField'
+
+const fields = [
+  {
+    id: 'name',
+    placeholder: 'Name',
+    type: 'input'
+  },
+  {
+    id: 'email',
+    placeholder: 'Email',
+    type: 'input'
+  },
+  {
+    id: 'message',
+    placeholder: 'Message',
+    type: 'textarea'
+  }
+]
+
+class Contact extends React.PureComponent {
+  state = {
+    isSending: false
+  }
+
+  onSubmit = values => {
+    console.log({ values }) // eslint-disable-line
+    this.setState({ isSending: true })
+
+    setTimeout(() => {
+      this.setState({ isSending: false })
+    }, 2000)
+  }
+
+  render() {
+    return (
+      <Form
+        onSubmit={this.onSubmit}
+        render={({ submitForm, errors }) => (
+          <form onSubmit={submitForm} id="contact">
+            {fields.map(field => (
+              <div className="form-field" key={field.id}>
+                <FormField {...field} />
+                <span className="error-message">
+                  {errors && errors[field.id] ? errors[field.id] : null}
+                </span>
+              </div>
+            ))}
+            <button type="submit" disabled={this.state.isSending}>
+              Send
+            </button>
+          </form>
+        )}
+      />
+    )
+  }
+}
 
 export default Contact
---
+++ web/src/components/FormField.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Text, TextArea } from 'react-form'
+import isEmail from 'validator/lib/isEmail'
+
+const FormField = ({ id, placeholder, type }) => {
+  const Field = type === 'input' ? Text : TextArea
+  const validate = value =>
+    id === 'email'
+      ? !value
+        ? `${placeholder} is required`
+        : !isEmail(value) ? 'Please enter a valid email' : null
+      : !value ? `${placeholder} is required` : null
+
+  return <Field field={id} placeholder={placeholder} validate={validate} />
+}
+
+FormField.propTypes = {
+  id: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired
+}
+
+export default FormField
