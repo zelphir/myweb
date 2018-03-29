35c1470053fb94392b314ce1fa885928112e0b81
Roberto Manzella <zelphir@gmail.com>
2018-03-29 12:33:44 +0100

    Update sidebar

--- web/src/components/SidebarContent.js
+++ web/src/components/SidebarContent.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-static'
 import { sidebarService } from 'react-sidebarjs'
-import PerfectScrollbar from 'perfect-scrollbar'
 
 import Stats from './Stats'
 import Info from './Info'
@@ -10,17 +9,6 @@ import Footer from './Footer'
 class Sidebar extends React.PureComponent {
   closeMenu = () => {
     sidebarService.toggle('sidebar')
-  }
-
-  ps = null
-
-  componentDidMount() {
-    this.ps = new PerfectScrollbar('#sidebar', { suppressScrollX: true })
-    this.ps.update()
-  }
-
-  componentWillUmnount() {
-    this.ps.destroy()
   }
 
   render() {
