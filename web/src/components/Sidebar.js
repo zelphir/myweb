9700de6e1bbf8bd10258bf9d04a11ec289ecc241
Roberto Manzella <zelphir@gmail.com>
2018-04-06 14:50:28 +0100

    Ad sidebar close button (mobile)

--- web/src/components/Sidebar.js
+++ web/src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { SidebarJS } from 'react-sidebarjs'
+import { SidebarJS, sidebarService } from 'react-sidebarjs'
 import { withRouter } from 'react-static'
 import { compose } from 'react-apollo'
 import { withMql } from '../lib/withMql'
@@ -9,24 +9,41 @@ import MobileHeader from './MobileHeader'
 
 import './Sidebar.scss'
 
-const Sidebar = ({ location, isMobile }) => {
-  const type = location.pathname.includes('/photos') ? 'photos' : 'dev'
+class Sidebar extends React.PureComponent {
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+    isMobile: PropTypes.bool
+  }
 
-  return isMobile ? (
-    <React.Fragment>
-      <SidebarJS sidebarjsName="sidebar">
-        <SidebarContent type={type} />
-      </SidebarJS>
-      <MobileHeader type={type} />
-    </React.Fragment>
-  ) : (
-    <SidebarContent type={type} />
-  )
-}
+  closeSidebar = e => {
+    e.preventDefault()
+    sidebarService.close('sidebar')
+  }
 
-Sidebar.propTypes = {
-  location: PropTypes.object.isRequired,
-  isMobile: PropTypes.bool
+  render() {
+    const { location, isMobile } = this.props
+    const type = location.pathname.includes('/photos') ? 'photos' : 'dev'
+
+    return isMobile ? (
+      <React.Fragment>
+        <SidebarJS
+          sidebarjsName="sidebar"
+          sidebarjsConfig={{
+            nativeSwipe: false,
+            nativeSwipeOpen: false
+          }}
+        >
+          <SidebarContent type={type} />
+          <span className="sidebar-close" onClick={this.closeSidebar}>
+            &times;
+          </span>
+        </SidebarJS>
+        <MobileHeader type={type} />
+      </React.Fragment>
+    ) : (
+      <SidebarContent type={type} />
+    )
+  }
 }
 
 export default compose(withMql, withRouter)(Sidebar)
