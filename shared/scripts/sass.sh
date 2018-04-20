23bd102996bea11508dc60d35d12265595a39220
Roberto Manzella <zelphir@gmail.com>
2018-04-20 14:54:04 +0100

    Old deps cleanup

--- shared/scripts/sass.sh
+++ shared/scripts/sass.sh
@@ -1,6 +1,5 @@
 node-sass \
   --quiet \
-  --importer "../node_modules/node-sass-json-importer/dist/node-sass-json-importer.js" \
   --include-path "./src" \
   --include-path "./src/assets/scss" \
   --recursive \
